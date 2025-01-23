7fc59b38f5ef3d302658483aba2f09493415b86f
gilsheps <gil.sheps@gmail.com>
2025-01-23 15:02:34 +0200

    Delete full_stack_final_project-cinema/.history directory

--- full_stack_final_project-cinema/.history/configs/models/userModel_20241217111028.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true, unique: true },
-  actionsAllowed: { type: Number, default: 10 },
-});
-
-const User = mongoose.model("User", userSchema);
-module.exports = User;
